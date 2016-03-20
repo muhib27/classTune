a9bb19535f470c445e1759a5e30ba751e89e5c87
Taufiq Ovi <ovioviovi@gmail.com>
2016-03-20 12:31:48 +0600

    1. Added multiple language support item image in right drawer 2. Modified strings in some areas.

--- app/src/main/java/com/classtune/app/schoolapp/viewhelpers/MyFragmentTabHost.java
+++ app/src/main/java/com/classtune/app/schoolapp/viewhelpers/MyFragmentTabHost.java
@@ -12,7 +12,7 @@ package com.classtune.app.schoolapp.viewhelpers;
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
+ * See the License for the specific multiple_lang_normal governing permissions and
  * limitations under the License.
  */
 
